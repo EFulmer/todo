092a69d12ae58bfa760544b6a7d5512dfb694689
Eric R. Fulmer <2952996+EFulmer@users.noreply.github.com>
2023-06-28 17:04:44 -0400

    BLD/FMT: add style and format checking tools and Make commands for them

--- tests/test_io.py
+++ tests/test_io.py
@@ -5,10 +5,10 @@ import todo.io
 
 def test_read_items_fails_with_bad_file_name():
     with pytest.raises(FileNotFoundError):
-        todo_gen = todo.io.read_items('this file does not exist.csv')
+        todo_gen = todo.io.read_items("this file does not exist.csv")
         next(todo_gen)
 
 
 def test_read_items_happy_case():
-    todo_gen = todo.io.read_items('tests/static/example.csv')
+    todo_gen = todo.io.read_items("tests/static/example.csv")
     next(todo_gen)
--- todo/main.py
+++ todo/main.py
@@ -4,11 +4,11 @@ from todo import io
 
 
 @click.command()
-@click.option('--file', help='To-Do list file.')
+@click.option("--file", help="To-Do list file.")
 def main(file):
     for x in io.read_items(file):
         print(x)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
