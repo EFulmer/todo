f96aaae4ecb9a920939be830ab86dbc2122153a0
Eric R. Fulmer <2952996+EFulmer@users.noreply.github.com>
2023-06-28 18:01:04 -0400

    fix tests

--- tests/test_io.py
+++ tests/test_io.py
@@ -12,3 +12,13 @@ def test_read_items_fails_with_bad_file_name():
 def test_read_items_happy_case():
     todo_gen = todo.io.read_items("tests/static/example.csv")
     next(todo_gen)
+
+
+def test_show_all():
+    todo_gen = todo.io.show_all("tests/static/example.csv")
+    assert len(list(todo_gen)) == 2
+
+
+def test_show_all():
+    todo_gen = todo.io.show_non_complete("tests/static/example.csv")
+    assert len(list(todo_gen)) == 1
--- todo/io.py
+++ todo/io.py
@@ -1,18 +1,36 @@
+"""Functions for reading and writing the To-Do list CSV."""
 import csv
+import dataclasses
+import datetime
 import pathlib
-from dataclasses import dataclass
 from typing import Generator
 
 
-@dataclass
+DATE_FORMAT = "%Y-%m-%d"
+strptime = lambda x: datetime.datetime.strptime(x, DATE_FORMAT).date()
+
+
+@dataclasses.dataclass
 class ToDoItem:
     _id: int
     name: str
-    date_created: str
-    complete: bool
+    date_created: datetime.date = dataclasses.field(default_factory=datetime.date.today)
+    complete: bool = False
 
     def __str__(self):
         return f"{self._id}. {self.name}"
+
+
+FIELDS = dataclasses.fields(ToDoItem)
+FIELD_NAMES = [f.name for f in FIELDS]
+
+
+def read_bool(s: str) -> bool:
+    map_ = {
+        "true": True,
+        "false": False,
+    }
+    return map_[s.lower()]
 
 
 def read_items(file_name: str) -> Generator[ToDoItem, None, None]:
@@ -30,6 +48,21 @@ def read_items(file_name: str) -> Generator[ToDoItem, None, None]:
     if not pathlib.Path(file_name).exists():
         raise FileNotFoundError(f"{file_name} does not exist")
     with open(file_name) as f:
-        reader = csv.reader(f)
+        reader = csv.DictReader(f, fieldnames=FIELD_NAMES)
         for row in reader:
-            yield ToDoItem(*row)
+            # TODO(Eric): get types working nicely; e.g. complete
+            # should get automatically converted to a boolean upon load
+            yield ToDoItem(
+                _id=row["_id"],
+                name=row["name"],
+                date_created=strptime(row["date_created"]),
+                complete=read_bool(row["complete"]),
+            )
+
+
+def show_all(file_name: str):
+    yield from (x for x in read_items(file_name))
+
+
+def show_non_complete(file_name: str):
+    yield from (x for x in read_items(file_name) if not x.complete)
